28d875c7763db3cf19f02a9e53b5daf270701be3
AhmedTarek73 <121679626+AhmedTarek73@users.noreply.github.com>
2022-12-30 21:13:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+{
+    int n1, n2, r;
+    Console.WriteLine("enter the frist number :");
+    n1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("enter the last number :");
+    n2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("the perfect number is :");
+    for (int i = n1; i <= n2; i++)
+    {
+        int factor = 0;
+        for (int z = 1; z <= n2; z++)
+        {
+            if ((i % z == 0) & (i != 1))
+            {
+                if (z == i)
+                {
+                    continue;
+
+                }
+                else
+                    factor += z;
+            }
+            else
+                continue;
+
+        }
+        if (factor == i)
+        {
+            Console.WriteLine(i);
+        }
+        else
+            factor = 0;
+
+
+    }
+
+}
+ 
